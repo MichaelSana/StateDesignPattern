30bb7c08dcbb91ab131ef89a9dcc61eea1f21a79
Michael Sana <77210893+MichaelSana@users.noreply.github.com>
2021-04-18 21:03:42 -0400

    Bug fix

--- HuluState.java
+++ HuluState.java
@@ -5,34 +5,32 @@ public class HuluState implements State {
 
     @Override
     public void pressHomebutton() {
-        System.out.println("Loading Home Screen...");
+        System.out.println("Loading Home Screen...\n");
         tv.setState(tv.getHomeState());
         
     }
 
     @Override
     public void pressNetflixButton() {
-        System.out.println("Loading Netflix...");
+        System.out.println("Loading Netflix...\n");
         tv.setState(tv.getNetflixState());
     }
 
     @Override
     public void pressHuluButton() {
-        System.out.println("TV is already on Hulu");
+        System.out.println("TV is already on Hulu\n");
         
     }
 
     @Override
     public void pressMovieButton() {
-        System.out.println("Loading Home Screen...");
-        tv.setState(tv.getHomeState());
+        System.out.println("Hulu Movies:\n- Cars\n - Cinderella\n- Wall-E\n- ET\n");
         
     }
 
     @Override
     public void pressTVButton() {
-        System.out.println("Loading Home Screen...");
-        tv.setState(tv.getHomeState());
+        System.out.println("Hulu TV Shows:\n- sesame street\n- care bears\n- loney tunes\n");
         
     }
     
--- NetflixState.java
+++ NetflixState.java
@@ -5,20 +5,20 @@ public class NetflixState implements State {
 
     @Override
     public void pressHomebutton() {
-        System.out.println("Loading the Home Screen...");
+        System.out.println("Loading the Home Screen...\n");
         tv.setState(tv.getHomeState());
         
     }
 
     @Override
     public void pressNetflixButton() {
-        System.out.println("TV is already on Netflix");
+        System.out.println("TV is already on Netflix\n");
         
     }
 
     @Override
     public void pressHuluButton() {
-        System.out.println("Loading Hulu...");
+        System.out.println("Loading Hulu...\n");
         tv.setState(tv.getHuluState());
 
         
@@ -26,14 +26,13 @@ public class NetflixState implements State {
 
     @Override
     public void pressMovieButton() {
-        System.out.println("Loading the Home Screen...");
-        tv.setState(tv.getHomeState());
+        System.out.println("Netflix Movies:\n - The Land Before Time\n - Frozen\n- The Little Mermaid\n- Ice Age\n");
+        
     }
 
     @Override
     public void pressTVButton() {
-        System.out.println("Loading the Home Screen...");
-        tv.setState(tv.getHomeState());
+        System.out.println("Netflix TV Shows:\n - Peppa Pig\n- My Little Pony\n- Garfield\n- Teenage Mutant Ninja Turtles\n");
     }
     
 }
